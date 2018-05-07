fa524ef559823b252d441731594b0e3c123ac419
peytr <peytr@mail.com>
2018-05-07 20:38:51 +1000

    Add offers form

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,11 +1,23 @@
 class OffersController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :set_request, only: [:show, :edit, :update, :delete]
 
   def new
     @offer = Offer.new
     @request = Request.find(params[:request_id])
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @request = Request.find(params[:request_id])
+
+    if @offer.save
+      flash[:notice] = 'Parts Offer has been submitted. We will notify you if you offer has been accepted.'
+      redirect_to root_path
+    else
+      flash[:alert] = 'Could not submit your parts offer.'
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def edit
@@ -15,9 +27,8 @@ class OffersController < ApplicationController
   end
 
   private
-
-  def set_request
-    @request = Request.find(params[:request_id])
+  def offer_params
+    params.require(:offer).permit(:message, :price, :image_data)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,26 @@
 Rails.application.routes.draw do
   
-  get 'offers/new'
-  get 'offers/edit'
-  get 'offers/show'
   root 'pages#home'
 
   devise_for :users
 
   get '/profile', to: 'profiles#show'
-
   post '/profile', to: 'profiles#create'
-
   get '/profile/edit', to: 'profiles#edit'
-
   patch '/profile', to: 'profiles#update'
 
+  post '/requests/:request_id/offers', to: 'offers#create'
+  get '/requests/:request_id/offers:id', to: 'offers#show'
 
   resources :requests do
     resources :offers
   end
 
   # get '/requests/index', to: 'requests#index'
-  
   # get '/requests/new', to: 'requests#new'
-
   # get '/request', to: 'requests#show'
-
   # post '/request', to: 'requests#create'
-
   # get '/request/edit', to: 'requests#edit'
-
   # patch '/request', to: 'requests#update'
 
 end
